a23a97c9f5dac742724f347949bd071b18b0fca8
Maximilian Alexander <mbalex99@gmail.com>
2017-01-26 00:52:07 -0800

    adding swift version

---
+++ MongoPredicate/MongoPredicate.h
@@ -0,0 +1,17 @@
+//
+//  MongoPredicate.h
+//  MongoPredicate
+//
+//  Created by Max Alexander on 1/25/17.
+//  Copyright © 2017 Max Alexander. All rights reserved.
+//
+
+//! Project version number for MongoPredicate.
+FOUNDATION_EXPORT double MongoPredicateVersionNumber;
+
+//! Project version string for MongoPredicate.
+FOUNDATION_EXPORT const unsigned char MongoPredicateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MongoPredicate/PublicHeader.h>
+
+
---
+++ MongoPredicateTests/MongoPredicateTests.m
@@ -0,0 +1,38 @@
+//
+//  MongoPredicateTests.m
+//  MongoPredicateTests
+//
+//  Created by Max Alexander on 1/25/17.
+//  Copyright © 2017 Max Alexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "MongoDBPredicateAdaptor.h"
+@interface MongoPredicateTests : XCTestCase
+
+@end
+
+@implementation MongoPredicateTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
