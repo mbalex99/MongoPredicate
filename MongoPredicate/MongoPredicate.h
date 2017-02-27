30d00edba8328489607b2128d8a339f6c17de791
Max Alexander <mbalex99@gmail.com>
2017-02-26 17:24:35 -0800

    adding mongopredicate with proper compile flags

---
+++ MongoPredicate/MongoPredicate.h
@@ -0,0 +1,19 @@
+//
+//  MongoPredicate.h
+//  MongoPredicate
+//
+//  Created by Max Alexander on 2/26/17.
+//  Copyright © 2017 Max Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MongoPredicate.
+FOUNDATION_EXPORT double MongoPredicateVersionNumber;
+
+//! Project version string for MongoPredicate.
+FOUNDATION_EXPORT const unsigned char MongoPredicateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MongoPredicate/PublicHeader.h>
+
+
